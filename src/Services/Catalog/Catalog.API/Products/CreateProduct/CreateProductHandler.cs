1eef264b3bb4a6e89d99e1bdee5fa3d40dd77b3d
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2024-11-02 13:38:50 -0300

    feat: Configuracao lib Marten e configuracao docker compose postgresql

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,12 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -24,10 +22,9 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
         
         // TODO
         // save to database
-        
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
         //return result
-        return new CreateProductResult(Guid.NewGuid());
-        
-        throw new NotImplementedException();
+        return new CreateProductResult(product.Id);
     }
 }
