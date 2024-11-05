f0407fd79059a1c50c0c06033860b2cd0ca0246b
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2024-11-04 23:32:46 -0300

    feat: Consulta de produto por Id

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<getProductByIdResult>;
+    public record getProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler
+        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, getProductByIdResult>
+    {
+        public async Task<getProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new getProductByIdResult(product);
+        }
+    }
+}
